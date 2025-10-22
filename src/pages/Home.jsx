84c6c6540a7d476a06adddf8661968619248b530
toska27 <nemanjatoskic58@gmail.com>
2025-10-22 17:00:46 +0200

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { collection, getDocs, updateDoc, doc } from "firebase/firestore";
+import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
 import FeedbackCard from "../components/FeedbackCard";
 
@@ -7,15 +7,20 @@ export default function Home() {
   const [feedbacks, setFeedbacks] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, "feedbacks"));
-      const items = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setFeedbacks(items);
+    const fetchFeedbacks = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "feedbacks"));
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setFeedbacks(data);
+      } catch (error) {
+        console.error("Greška prilikom učitavanja:", error);
+      }
     };
-    fetchData();
+
+    fetchFeedbacks();
   }, []);
 
   const handleLike = async (id, likes) => {
@@ -32,11 +37,11 @@ export default function Home() {
     <div className="max-w-5xl mx-auto px-4 py-8">
       <h2 className="text-3xl font-semibold mb-6">User impressions:</h2>
       {feedbacks.length > 0 ? (
-        feedbacks.map((f) => {
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <FeedbackCard key={f.id} feedback={f} onLike={handleLike} />;
-          </div>;
-        })
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {feedbacks.map((f) => (
+            <FeedbackCard key={f.id} feedback={f} onLike={handleLike} />
+          ))}
+        </div>
       ) : (
         <h4 className="text-2xl font-medium mx-auto mt-5">
           No impressions yet.
